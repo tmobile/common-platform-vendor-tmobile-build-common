7a255230116650e48ecae298418c94373752efcf
Dirk Sigurdson <dirk.sigurdson@t-mobile.com>
2009-08-05 18:30:25 -0700

    added the providers subdir

--- Android.mk
+++ Android.mk
@@ -1,3 +1,4 @@
 include $(call all-makefiles-under,vendor/tmobile/apps)
+include $(call all-makefiles-under,vendro/tmobile/providers)
 include $(call all-makefiles-under,vendor/tmobile/samples)
 include $(call all-makefiles-under,vendor/tmobile/themes)
